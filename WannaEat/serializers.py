89e79e5a5de9826487993455dda17068128463bc
Nikita <glaider102@mail.ru>
2020-09-22 21:01:09 +0300

    add login token access

--- WannaEat/serializers.py
+++ WannaEat/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+
 from .models import Receipt
+from django.contrib.auth.models import User
 
 
 class ReceiptListSerializer(serializers.ModelSerializer):
@@ -8,3 +11,20 @@ class ReceiptListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Receipt
         fields = ("title", "description", "image", "cooking_steps")
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        Token.objects.create(user=user)
+        return user
