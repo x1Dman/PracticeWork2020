98ac7c41f69e67722ce0ef4683e377ea7ee5b5d4
x1Dman <glaider102@mail.ru>
2020-09-25 02:13:45 +0300

    upd for dot

--- untitled/settings.py
+++ untitled/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'WannaEat',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
